8f3cd5a70dda2fce72b37e5c54e6e2a7f30e7014
Caleb Watt <caleb@ubuntu.(none)>
2016-09-09 08:24:10 -0700

    Ex1 and Ex2 complete

---
+++ learnCTheHardWay/ex1Break.c
@@ -0,0 +1,6 @@
+//#include <stdio.h>
+int main()
+{
+//printf("You are %d miles away.\n", 100);
+return 0;
+}
---
+++ learnCTheHardWay/ex1Ec.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+    printf("Five additional lines:\nNew line 2\nNew line 3\n");
+    printf("fourth new line.\n");
+    printf("final new line, per EC part 2\n");
+
+    return 0;
+}
